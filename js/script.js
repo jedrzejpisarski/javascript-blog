04e336b65faaf95790b4991050adc21e2d5a8997
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-02-19 20:15:50 +0100

    Js_script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(){
+const titleClickHandler = function(){
 
     console.log (event);
     
@@ -11,9 +12,21 @@
 
   /* remove class 'active' from all article links  */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.articles a.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
