6f3125fc87a0d964155b908b894060d0f7e921b2
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-02-19 20:38:29 +0100

    Js_script

--- js/script.js
+++ js/script.js
@@ -4,37 +4,30 @@
     console.log('links:', links);
   });*/
 
-const titleClickHandler = function(){
-
-    console.log (event);
-    
+  const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-
-  /* remove class 'active' from all article links  */
-
-const activeLinks = document.querySelectorAll('.titles a.active');
-
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
-const activeArticles = document.querySelectorAll('.articles a.active');
-
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
-
+  
+    /* [DONE] remove class 'active' from all article links  */
+  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+  
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+  
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+    console.log('clickedElement:', clickedElement);
+  
+    /* [DONE] remove class 'active' from all articles */
+  
+    /* get 'href' attribute from the clicked link */
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
+  }
   
   const links = document.querySelectorAll('.titles a');
   
