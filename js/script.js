1dd48270511b5b690b821dc1a2d9a037f5653e9d
jedrzejpisarski <60627936+jedrzejpisarski@users.noreply.github.com>
2020-02-26 17:52:56 +0100

    Generate tags add

--- js/script.js
+++ js/script.js
@@ -30,3 +30,44 @@ function titleClickHandler() {
 }
 
 generateTitleLinks();
+
+/*GENERATE TAGS*/
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+const optArticleTagsSelector('.post-tags .list')
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll('.posts .post');
+
+  /* START LOOP: for every article: */
+  for(let article of articles) {
+    const article = article.querySelector('.post-title').innerHTML;
+    /* find tags wrapper */
+    const optArticleTagsSelector = article.querySelectorAll('.post-tags .list .list-horizontal');    
+    
+    /* make html variable with empty string */
+    let html = html''
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+  }
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
+
